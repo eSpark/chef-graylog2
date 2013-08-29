231ffb305baa801f51067efad3850dfcb25a3034
Alex Koppel <alex@alexkoppel.com>
2013-08-29 11:57:22 -0500

    Update graylog2 web_interface version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,7 @@
 default[:graylog2][:repo] = 'http://download.graylog2.org'
 default[:graylog2][:basedir] = "/var/graylog2"
 default[:graylog2][:server][:version] = "0.11.0"
-default[:graylog2][:web_interface][:version] = "0.11.0"
-default[:graylog2][:web_interface][:listen_port] = 4500 # if this is not 80 you need to override default['apache']['listen_ports'] = [ "80" ] 
+default[:graylog2][:web_interface][:version] = "0.12.0"
 
 default[:graylog2][:elasticsearch][:repo] = 'http://download.elasticsearch.org/elasticsearch/elasticsearch'
 default[:graylog2][:elasticsearch][:version] ="0.20.6"
