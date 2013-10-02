33139a3d474f8b2ca759a75f6954cb7122a49326
Alex Koppel <alex@alexkoppel.com>
2013-10-02 14:38:36 -0500

    Clean up metadata

--- metadata.rb
+++ metadata.rb
@@ -10,10 +10,8 @@ recipe            "graylog2", "Installs and configures Graylog2"
 supports "ubuntu"
 
 # OpsCode cookbook dependencies
-depends "apt"     # http://community.opscode.com/cookbooks/apt
+depends "apt"        # http://community.opscode.com/cookbooks/apt
 depends "rbenv"      # http://community.opscode.com/cookbooks/rbenv
 depends "ruby_build" # http://community.opscode.com/cookbooks/ruby_build
-depends "apache2" # http://community.opscode.com/cookbooks/apache2
-depends "apache2" # http://community.opscode.com/cookbooks/apache2
 depends "elasticsearch"
-depends "mongodb" # http://community.opscode.com/cookbooks/mongodb
+depends "mongodb"    # http://community.opscode.com/cookbooks/mongodb
